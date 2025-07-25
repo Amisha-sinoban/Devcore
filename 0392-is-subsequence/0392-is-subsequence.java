7d95d2d415f6f00505ff9b5b773cf8d17b9a87a8
Amisha singh <140898197+Amisha-sinoban@users.noreply.github.com>
2025-07-25 22:40:31 +0530

    Time: 2 ms (68.48%), Space: 41.4 MB (86.26%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isSubsequence("abc", "ahbgdc")); // true
+        System.out.println(sol.isSubsequence("axc", "ahbgdc")); // false
+    }
+}
