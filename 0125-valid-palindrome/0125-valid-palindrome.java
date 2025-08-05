751faee195df2ef39f3f1e416d7b27edc8a9172b
Amisha singh <140898197+Amisha-sinoban@users.noreply.github.com>
2025-08-05 23:33:21 +0530

    Time: 2 ms (98.52%), Space: 42.8 MB (90.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       
-        int left = 0, right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
         while (left < right) {
-            // Skip non-alphanumeric characters
+            // Move left pointer if not alphanumeric
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
+
+            // Move right pointer if not alphanumeric
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            // Compare characters in lowercase
+            // Compare lowercase characters
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
@@ -24,5 +26,3 @@ class Solution {
         return true;
     }
 }
-
-    
